1cfa08857aaff5f13a349ed4c422fb36519e7797
Nelson <xhyskoax@gmail.com>
2026-06-05 21:33:05 -0400

    Se avanza en lógica de sendmoney, faltando sólo agregar nuevos contactos (no exigido)

--- m2/proyecto-l5/assets/js/sendmoney.js
+++ m2/proyecto-l5/assets/js/sendmoney.js
@@ -26,13 +26,12 @@ const contactos = [
     },
 ];
 
-
 //FUNCIÓN QUE SE VA A ENCARGAR DE CARGAR LOS CONTACTOS EN LA UL DEL DOM
 function cargarContactos(listaContactos){
 
     let acumuladorLi = "";
 
-    for (const contacto of contactos) {
+    for (const contacto of listaContactos) {
 
         //DESECTRUCTURAR LAS PROPIEDADES DEL OBJETO
         let { id, nombre, cbu, alias, banco } = contacto;
@@ -52,7 +51,6 @@ function cargarContactos(listaContactos){
     document.getElementById("listaContactos").innerHTML = acumuladorLi;
 };
 
-
 //FUNCIÓN INICIAL / PRINCIAL
 
 function main(){
@@ -60,3 +58,95 @@ function main(){
 };
 
 main();
+
+//LÓGICA BUSCAR CONTACTO CON FILTRO DE CONTACTOS
+
+const buscarContactoEl = document.getElementById("buscarContacto");
+
+//AGREGAMOS EL EVENTO INPUT AL ELEMENTO
+
+buscarContactoEl.addEventListener("input", function(event){
+    event.preventDefault();
+
+    let textoBusqueda = buscarContactoEl.value;
+
+    //DEJAMO EL TEXTO EL INPUT EN MINÚSCULAS
+    textoBusqueda = textoBusqueda.toLowerCase();
+
+    //FILTRAR LOS CONTACTOS QUE COINCIDAN POR EL TEXTO BUSCADO
+
+    let contactosFiltrados = contactos.filter(function(contacto){
+        //DESECTRUCTURAR LAS PROPIEDADES DEL OBJETO
+        let {nombre, cbu, alias, banco } = contacto;
+
+        nombre = nombre.toLowerCase();
+        alias = alias.toLowerCase();
+        banco = banco.toLowerCase();
+
+        let reglaNombre = nombre.includes(textoBusqueda);
+        let reglaAlias = alias.includes(textoBusqueda);
+        let reglaCbu = cbu.includes(textoBusqueda);
+        let reglaBanco = banco.includes(textoBusqueda);
+
+        if(reglaNombre || reglaAlias || reglaCbu || reglaBanco){
+            return contacto;
+        }else{
+            return false;
+        }
+    });
+
+    //UNA VEZ FILTRADOS LOS CONTACTOS LOS ENVIAMOS A LA FUNCIÓN DE CARGAR CONTACTOS
+    cargarContactos(contactosFiltrados);
+});
+
+//LÓGICA DE ENVIAR DINERO A UN USUARIO SELECCIONADO
+
+const formSendMoneyEl = document.getElementById("formSendMoney");
+
+//AGREGAMOS EL EVENTO SUBMIT
+
+formSendMoneyEl.addEventListener("submit", function(event){
+
+    //DEBEMOS PREVENIR LAS ACCIONES POR DEFECTO
+    event.preventDefault();
+
+    //CAPTURAMOS LA DATA DEL FORMULARIO
+    let dataFormulario = new FormData(formSendMoneyEl);
+
+    let monto = dataFormulario.get("monto");
+
+    monto = Number(monto);
+
+    //PREGUNTAMOS AL USUARIO SI ESTÁ SEGURO DE TRANSFERIR
+    let confirmacion = confirm(`¿Está seguro de transferir la suma de: $ ${monto.toLocaleString("es-CL")}?`);
+
+    if(!confirmacion){
+        //EL RETURN DETIENE LA EJECUCIÓN DEL RESTO DE CÓDIGO
+        return;
+    }
+
+
+    let cbuContacto = dataFormulario.get("contacto");
+
+    //VALIDAR SI EXISTE SALDO DISPONIBLE Y DESCONTAR DE SER POSIBLE
+
+    if(balance >= monto){
+        //EN ESTE CASO PUEDO TRANSFERIR
+        balance = balance - monto;
+        let textoMensaje = 
+        `Se ha transferido correctamente la suma de: $ ${monto.toLocaleString("es-CL")} a la cuenta\nN° ${cbuContacto}.
+        \nSu nuevo saldo es de: ${balance.toLocaleString("es-CL")}
+        `;
+        alert(textoMensaje);
+
+        //ACTUALIZAMOS EL BALANCE EN EL SPAN DEL DOM
+        balanceEl.textContent = balance.toLocaleString("es-CL");
+
+        //limpiamos el formulario
+        formSendMoneyEl.reset();
+        cargarContactos(contactos);
+
+    }else{
+        alert("Usted no disponible de saldo suficiente");
+    }
+});
